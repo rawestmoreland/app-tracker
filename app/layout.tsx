173818c40d9a03c7d1a3510603041dbd11068340
Richard Westmoreland <rawestmoreland@gmail.com>
2025-08-10 17:29:02 +0100

    chore: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           <div className='min-h-full'>
             <Header />
             <main>
-              <div className='mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8'>
+              <div className='mx-auto container px-4 py-8 sm:px-6 lg:px-8'>
                 {children}
               </div>
             </main>
