df623962ead909514ef68ae2b9985fbf4e08dbc2
Richard W <rawestmoreland@gmail.com>
2025-08-12 10:32:36 +0100

    Delete lib/scraper directory

--- lib/scraper/index.ts
+++
@@ -1,3 +0,0 @@
-export { LinkedInCompanyScraper, linkedinScraper } from './linkedin-scraper.js';
-export type { LinkedInCompanyData, ScrapingResult, ScrapingOptions } from './types.js';
-export { ScrapingError } from './types.js';
