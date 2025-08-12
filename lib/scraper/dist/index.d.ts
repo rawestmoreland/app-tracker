e8dfeb0c02d9d9f081602dce2a569591503e0846
Richard Westmoreland <rawestmoreland@gmail.com>
2025-08-12 10:25:38 +0100

    feat: scraper

--- lib/scraper/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { LinkedInCompanyScraper, linkedinScraper } from './linkedin-scraper.js';
-export type { LinkedInCompanyData, ScrapingResult, ScrapingOptions } from './types.js';
-export { ScrapingError } from './types.js';
-//# sourceMappingURL=index.d.ts.map
