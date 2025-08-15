cf9a8604cf628f78fc1c11b94e821a6221404a4c
Richard Westmoreland <rawestmoreland@gmail.com>
2025-08-15 11:56:42 +0100

    feat: new icon

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { UserButton, useAuth } from '@clerk/nextjs';
-import Link from 'next/link';
-import Image from 'next/image';
-import { cn } from '@/lib/utils';
-import { usePathname } from 'next/navigation';
-import { Button } from '@/components/ui/button';
-import { CoffeeIcon } from 'lucide-react';
+import { UserButton, useAuth } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { CoffeeIcon, LaptopMinimalCheckIcon } from "lucide-react";
 
 export function Header() {
   const { isSignedIn, isLoaded } = useAuth();
@@ -20,59 +20,58 @@ export function Header() {
   if (!isLoaded) return null;
 
   return (
-    <nav className='border-b border-gray-200 bg-white sticky top-0 z-50'>
-      <div className='mx-auto container px-4 sm:px-6 lg:px-8'>
-        <div className='flex h-16 justify-between'>
-          <div className='flex'>
-            <div className='flex shrink-0 items-center'>
-              <Image
-                alt='App Track'
-                src='https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600'
-                className='h-8 w-auto'
-                width={32}
-                height={32}
-              />
+    <nav className="sticky top-0 z-50 border-b border-gray-200 bg-white">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 justify-between">
+          <div className="flex">
+            <div className="flex h-full items-center">
+              <Link
+                href="/dashboard"
+                className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-500"
+              >
+                <LaptopMinimalCheckIcon className="h-5 w-5" color="white" />
+              </Link>
             </div>
             {isSignedIn && (
-              <div className='hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8'>
+              <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
                 <Link
-                  href='/dashboard'
+                  href="/dashboard"
                   aria-current={
-                    isCurrentPath('/dashboard') ? 'page' : undefined
+                    isCurrentPath("/dashboard") ? "page" : undefined
                   }
                   className={cn(
-                    isCurrentPath('/dashboard')
-                      ? 'border-indigo-600 text-gray-900'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                    'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'
+                    isCurrentPath("/dashboard")
+                      ? "border-indigo-600 text-gray-900"
+                      : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                    "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium",
                   )}
                 >
                   My Applications
                 </Link>
                 <Link
-                  href='/dashboard/companies'
+                  href="/dashboard/companies"
                   aria-current={
-                    isCurrentPath('/dashboard/companies') ? 'page' : undefined
+                    isCurrentPath("/dashboard/companies") ? "page" : undefined
                   }
                   className={cn(
-                    isCurrentPath('/dashboard/companies')
-                      ? 'border-indigo-600 text-gray-900'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                    'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'
+                    isCurrentPath("/dashboard/companies")
+                      ? "border-indigo-600 text-gray-900"
+                      : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                    "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium",
                   )}
                 >
                   My Companies
                 </Link>
                 <Link
-                  href='/dashboard/interviews'
+                  href="/dashboard/interviews"
                   aria-current={
-                    isCurrentPath('/dashboard/interviews') ? 'page' : undefined
+                    isCurrentPath("/dashboard/interviews") ? "page" : undefined
                   }
                   className={cn(
-                    isCurrentPath('/dashboard/interviews')
-                      ? 'border-indigo-600 text-gray-900'
-                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                    'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'
+                    isCurrentPath("/dashboard/interviews")
+                      ? "border-indigo-600 text-gray-900"
+                      : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                    "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium",
                   )}
                 >
                   My Interviews
@@ -80,13 +79,13 @@ export function Header() {
               </div>
             )}
           </div>
-          <div className='hidden sm:ml-6 sm:flex sm:items-center gap-4'>
-            <Button variant='outline' asChild>
+          <div className="hidden gap-4 sm:ml-6 sm:flex sm:items-center">
+            <Button variant="outline" asChild>
               <Link
-                href='https://buymeacoffee.com/westmorelandcreative'
-                target='_blank'
+                href="https://buymeacoffee.com/westmorelandcreative"
+                target="_blank"
               >
-                <CoffeeIcon className='w-5 h-5' />
+                <CoffeeIcon className="h-5 w-5" />
                 Donate
               </Link>
             </Button>
