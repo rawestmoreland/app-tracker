a0ed210371c59762f8ad22ab59526ae5ac93bdc5
Richard Westmoreland <rawestmoreland@gmail.com>
2025-08-22 13:20:59 +0100

    fix: onboaarding

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -15,6 +15,8 @@ const isPublicRoute = createRouteMatcher([
 export default clerkMiddleware(async (auth, req) => {
   const { userId, sessionClaims, redirectToSignIn } = await auth();
 
+  console.log('sessionClaims', sessionClaims);
+
   // For users visiting /onboarding, don't try to redirect
   if (userId && isOnboardingRoute(req)) {
     return NextResponse.next();
@@ -32,11 +34,7 @@ export default clerkMiddleware(async (auth, req) => {
 
   // Catch users who do not have `onboardingComplete: true` in their publicMetadata
   // Redirect them to the /onboarding route to complete onboarding
-  if (
-    userId &&
-    !(sessionClaims?.metadata as { onboardingComplete: boolean })
-      ?.onboardingComplete
-  ) {
+  if (userId && !sessionClaims?.metadata?.onboardingComplete) {
     const onboardingUrl = new URL('/onboarding', req.url);
     return NextResponse.redirect(onboardingUrl);
   }
