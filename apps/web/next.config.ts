4386012037ea4139bc1b21e276b86018a5974586
Richard Westmoreland <rawestmoreland@gmail.com>
2025-09-05 14:26:19 +0100

    feat: wellfound images

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'tailwindcss.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'wellfound.com',
+      },
     ],
   },
 };
